cc0b08d1170f574f72e4d1eddcdacf1f8b12e449
MaheswaraReddy <maheswarareddy.pr@gmail.com>
2022-07-06 21:10:44 +0530

    OpenCV Basic Operations
    Dilation, Erosion and Edge detection using OepnCV

---
+++ OpenCV_BasicOperations_2.py
@@ -0,0 +1,57 @@
+import imutils
+import cv2
+
+image = cv2.imread(r"D:\Study\MyML\OpenCV/Tetris_Blocks.jpg")
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+# convert the image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray", gray)
+cv2.waitKey(0)
+
+# applying edge detection we can find the outlines of objects in
+# images
+edged = cv2.Canny(gray, 30, 150)
+cv2.imshow("Edged", edged)
+cv2.waitKey(0)
+
+thresh = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY_INV)[1]
+cv2.imshow("Thresh", thresh)
+cv2.waitKey(0)
+
+# find contours (i.e., outlines) of the foreground objects in the
+# thresholded image
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+output = image.copy()
+# loop over the contours
+for c in cnts:
+	# draw each contour on the output image with a 3px thick purple
+	# outline, then display the output contours one at a time
+	cv2.drawContours(output, [c], -1, (240, 0, 159), 3)
+	cv2.imshow("Contours", output)
+	cv2.waitKey(0)
+# draw the total number of contours found in purple
+text = "I found {} objects!".format(len(cnts))
+cv2.putText(output, text, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+            (240, 0, 159), 2)
+cv2.imshow("Contours", output)
+cv2.waitKey(0)
+
+mask = thresh.copy()
+mask = cv2.erode(mask, None, iterations=5)
+cv2.imshow("Eroded", mask)
+cv2.waitKey(0)
+
+# similarly, dilations can increase the size of the ground objects
+mask = thresh.copy()
+mask = cv2.dilate(mask, None, iterations=5)
+cv2.imshow("Dilated", mask)
+cv2.waitKey(0)
+
+mask = thresh.copy()
+output = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow("Output", output)
+cv2.waitKey(0)
